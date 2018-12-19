d13c93f3b979a60b0a1cef092ddfbb37dbb1fe21
Alec <alecdifederico@gmail.com>
2018-12-19 03:52:30 -0500

    created simplified version of React application

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,30 @@
 const path = require('path');
-const webpack = require('webpack');
 
 
 module.exports = {
-  entry: ['babel-polyfill', './src/index.js'], // Where the application starts
+  entry: './src/index.js',
+  output: {
+    publicPath: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/, // Transform ES6 and JSX files to javascript
-        loader: 'babel-loader',
-        options: { 
-          presets: ['env'],
-          plugins: ['transform-class-properties']
-        },
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
       },
       {
-        test: /\.css$/, // Rule for processing CSS
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-  resolve: { extensions: ['*', '.js', '.jsx'] }, // Allows us to import modules without needing to add extensions
-  output: { // Where to put the bundled code
-    publicPath: path.resolve(__dirname, 'dist'), // What directory the bundle goes in
-    filename: 'bundle.js',
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   },
   devServer: {
-    contentBase: path.join(__dirname, 'public'), // Location of static files
+    contentBase: path.join(__dirname, 'public'),
     port: 3000,
-    publicPath: 'http://localhost:3000/dist', // Where the bundled code is
-  },
-  plugins: [new webpack.HotModuleReplacementPlugin()], // Reloads server with a code change
+    publicPath: 'http://localhost:3000/dist'
+  }
 };
